471571c542e49e6c2c9fdd1d24a41d0e13449464
DaddyPluM <103752309+DaddyPluM@users.noreply.github.com>
2023-01-07 15:29:21 +0100

    Added Computer Keypad Support

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Calc-Source
-This is a caluclator I made using Love2D in 2 days.
+This is a caluclator I made using Love2D in 2 days but I continued development on it.
 
-The calculator can be used fully with a mouse (except delete), but partially with a keyboard because I can't figure out how to make "+" and  "+" work
+The calculator can be used fully with a mouse (except delete), but partially with a keyboard because I can't figure out how to make "+" work
 
 It is a very simple calculator
+### Now With Keypad Support
+
+My code commenting skills aren't hat good
 
 Enjoy :)
--- main.lua
+++ main.lua
@@ -5,17 +5,31 @@ function love.load()
     row_start = 250     --The point where the keypad starts
     button_size = 120
     display = "0"   --What the user will see (The Screen). It will contain all the calculations
-    keypad = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "/", "."}
+    calc_keypad = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "/", "."}
+    pc_keypad = {"kp1", "kp2", "kp3", "kp4", "kp5", "kp6", "kp7", "kp8", "kp9", "kp0", "kp+", "kp-", "kp/", "kp*","kp."}
     love.graphics.setNewFont(60)    --Sets font size
-
-    row = {     --This table contains about the buttons for each row
-        one = {7, 8, 9, "c","OFF"},
+     row = {     --This table contains about the buttons for each row
+        one = {7, 8, 9, "C","OFF"},
         two = {4, 5,6, "+","-"},
-        three = {1, 2, 3, "*","/"},
+        three = {1, 2, 3, "x","÷"},
         four = {0, ".","^","="}
     }
 
     function click(v)   --Handles the keypad input
+        if v == "x" then    --Computers don't use "x" for multiplication
+            v = "*"
+        end
+        if v == "÷" then    --Computers don't use "÷" for multiplication
+            v = "/"
+        end
+        if v == "C" then
+            clear()
+            v = "0"
+        end
+        if v == "=" then
+            equals()
+            v = ""
+        end
         if display == "0" then
             display = ""
             display = display..v
@@ -29,7 +43,7 @@ function love.load()
     end
 
     function equals()   --Solve equation on screen
-        local result = load("return "..display)()   --I can't eplain what this does properly because I don't understand it so you will need to look it up for help
+        local result = load("return "..display)()   --This is how the calculator calculates. I can't explain it properly. Load turns a string (display) into code that is then executed. E.g. if display = "3+3", load will turn it into code and execute 3+3
         display = result
     end
 
@@ -56,11 +70,10 @@ function love.load()
     --[[This makes loading buttons easier
     It reads through a table and for every value, it gets its position in the table and the value itself]]
     for i, v in ipairs(row.one) do
-        if i ~= 5 and  i ~= 4 then      --We need this for buttons that don't display onto the screen e.g OFF
+        if i ~= 5 then      --We need this for buttons that don't display onto the screen e.g OFF
             row.one[i] = button(tostring(v), click, v )
         else
             row.one[5] = button("OFF", off)     --Special buttons
-            row.one[4] = button("c", clear)
         end
     end
     for i, v in ipairs(row.two) do
@@ -70,11 +83,7 @@ function love.load()
         row.three[i] = button(tostring(v), click, v)
     end
     for i, v in ipairs(row.four) do
-        if i ~= 4 then
             row.four[i] = button(tostring(v), click, v)
-        else
-            row.four[4] = button("=", equals)
-        end
     end
 end
 
@@ -84,9 +93,9 @@ function love.draw()
     love.graphics.rectangle("fill",0,0,win_width-10,240)
     --The same system we used for loading the buttons but now used for drawing them instead
     for i, v in ipairs(row.one) do
-        if i ~= 5 then  --This is for  special buttons
+        if i ~= 5 then  --Checks if the button it is about to draw is not the OFF button
             row.one[i]:draw((i-.9)*170, row_start)
-        else
+        else    --If it is the OFF button
         row.one[5]:draw((5-.9)*170, row_start,20,nil,.5,0,0,"fill")     --Because the OFF buttons is a different colour, we need to pass in some extra parameters
         end
     end
@@ -96,44 +105,53 @@ function love.draw()
     for i, v in ipairs(row.three) do
         row.three[i]:draw((i-.9)*170, row_start+260)
     end
-    --This row doesn't use the system because
     for i, v in ipairs(row.four) do
-    row.four[i]:draw(((i+1)-.9)*170, row_start+390)
+        row.four[i]:draw(((i+.5)-.9)*170, row_start+390)
     end
 
-    love.graphics.print(display,0,80)    --Displaying numbers onto the screen
+    love.graphics.print(display,0,80)    --Displays numbers onto the screen
 end
 
 
-function love.keypressed(key)   --Check which key on the keyboard was pressed
+function love.keypressed(key)   --Checks which key on the keyboard was pressed
     if key == "escape" then
         off()
     end
-    if key == "return"
-    or
-    key == "=" then
+    if key == "return" or key == "="  or key == "kpenter" then
         equals()
     end
     if key == "c" then
-        display = "0"
+        clear()
+    end
+    if key == "x" then
+        display = display.."*"
     end
     if key == "backspace" then
-        if display ~= "0"  and string.len(display) > 1 then     --Find the last character in the string "display" and removes it
+        if display ~= "0"  and string.len(display) > 1 then     --Finds the last character in the string "display" and removes it
                 display = display:sub(1, -2)
         else
             display = "0"
         end
     end
-    for k, v in pairs(keypad) do   --This links the keypad to the keyboard
+    for k, v in pairs(calc_keypad) do   --Links the calculator's keypad to the keyboard
         if key == tostring(v) then
             if display == "0" then
                     display = ""
-                display = display..v
+                    display = display..v
             else
                 display = display..v
             end
         end
     end
-    display = tostring(display)     --The system set up for deleting doesn't work on numbers, only on strings.
+    for i,v in pairs(pc_keypad) do      --Links the calculator's keyapd to the keyboard's keypad
+        if key == v then
+            key = string.gsub(v, "kp", "")      --If the 9 key on the keyboard's keypad was pressed, it will detect it as "kp9" was pressed. This line of code removes the first two characters, kp, from the string. Remove this line and see what I mean.
+            if display == "0" then
+                    display = ""
+                    display = display..key
+            else
+                display = display..key
+            end
+        end
+    end
 end
-
